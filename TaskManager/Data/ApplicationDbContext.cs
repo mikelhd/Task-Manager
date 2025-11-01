591346330c1eb1fa9ef649ef25793104db72019c
m.haddad <m.haddad@POUYA-HADDAD>
2025-11-01 13:51:26 +0330

    Database settings

--- TaskManager/Data/ApplicationDbContext.cs
+++ TaskManager/Data/ApplicationDbContext.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace TaskManager.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly SqlConnection
-        public ApplicationDbContext(DbContextOptions options) : base(options)
+        private readonly SqlConnection _sqlConnection;
+        public ApplicationDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
         {
-
+            _sqlConnection = new SqlConnection(configuration.GetConnectionString("ApplicationDBContext"));
         }
     }
 }
