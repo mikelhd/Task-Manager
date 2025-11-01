9d7cad7006824f8ed0fa9046a49e68c325a2f288
m.haddad <m.haddad@POUYA-HADDAD>
2025-11-01 13:44:05 +0330

    Initial clean commit

---
+++ TaskManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly SqlConnection
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
